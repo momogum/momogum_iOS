74947a18fe062088d8a2b92848a56385e04d92c8
Kim Ui Taek <kut7728@hanyang.ac.kr>
2025-01-06 22:25:35 +0900

    Initial Commit

---
+++ momogum/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  momogum
+//
+//  Created by nelime on 1/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ momogum/momogumApp.swift
@@ -0,0 +1,32 @@
+//
+//  momogumApp.swift
+//  momogum
+//
+//  Created by nelime on 1/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct momogumApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
