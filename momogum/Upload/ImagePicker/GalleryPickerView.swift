8a2193553f605d4fa33eea452766364bf58bbbe5
dori <rladbswls_06@naver.com>
2025-01-21 15:27:23 +0900

    Feat] #8 업로드 뷰 초기설정

---
+++ momogum/Upload/ImagePicker/GalleryPickerView.swift
@@ -0,0 +1,19 @@
+//
+//  GalleryPickerView.swift
+//  momogum
+//
+//  Created by 김윤진 on 1/21/25.
+//
+
+import SwiftUI
+
+struct GalleryPickerView: View {
+    @Binding var tabIndex: Int
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryPickerView(tabIndex: .constant(1))
+}
