feaaa5fef9a22f794a5766f1a3cea3035bef4376
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-01-21 17:52:12 +0900

    [fix] #15 이미지 수정 탭 뷰 수정

--- momogum/Upload/ImageEditor/ImageEditorVIew.swift
+++ momogum/Upload/ImageEditor/ImageEditorVIew.swift
@@ -4,61 +4,45 @@
 //
 //  Created by 조승연 on 1/17/25.
 //
-
 import SwiftUI
 
 struct ImageEditorView: View {
     var image: UIImage
-    @Binding var tabIndex: Int // 탭 인덱스를 바인딩으로 받아서 TabView를 전환
-    @Environment(\.presentationMode) var presentationMode // 뒤로 가기 기능을 위한 환경 변수
 
     var body: some View {
-        ZStack(alignment: .top) {
-            VStack {
-                // Custom Header
-                HStack {
-                    // Back Button
-                    Button(action: {
-                        tabIndex = 1 // 업로드 탭으로 이동
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .foregroundColor(.black)
-                            .font(.title2)
-                    }
-
-                    Spacer()
-
-                    // Close Button
-                    Button(action: {
-                        tabIndex = 0 // 홈 탭으로 이동
-                    }) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.black)
-                            .font(.title2)
-                    }
+        VStack {
+            HStack {
+                Button(action: {
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.black)
+                        .font(.title2)
                 }
-                .padding(.horizontal, 16)
-                .frame(height: 60)
-                .background(Color.white)
 
                 Spacer()
-            }
-            .zIndex(1)
 
-            // Main content: Selected image
-            VStack {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                Spacer()
+                Button(action: {
+                }) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.black)
+                        .font(.title2)
+                }
             }
-            .zIndex(0)
+            .padding(.horizontal, 16)
+            .frame(height: 60)
+            .background(Color.white)
+
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .padding()
+
+            Spacer()
         }
-        .navigationBarHidden(true) // 네비게이션 바 숨김
+        .navigationBarHidden(true)
     }
 }
 
 #Preview {
-    ImageEditorView(image: UIImage(systemName: "photo")!, tabIndex: .constant(1)) 
+    ImageEditorView(image: UIImage(systemName: "photo") ?? UIImage())
 }
