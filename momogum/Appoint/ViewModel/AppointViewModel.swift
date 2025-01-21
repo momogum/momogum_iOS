e508c7ef6038659b613a9418489688a5c364e44e
Kim Ui Taek <kut7728@hanyang.ac.kr>
2025-01-21 17:48:28 +0900

    Create AppointViewModel.swift

---
+++ momogum/Appoint/ViewModel/AppointViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AppointViewModel.swift
+//  momogum
+//
+//  Created by nelime on 1/21/25.
+//
+
+import SwiftUI
+
+struct AppointViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppointViewModel()
+}
