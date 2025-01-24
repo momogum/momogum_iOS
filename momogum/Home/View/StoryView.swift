68c8636dc7c68614d2e291adbf6ea2fdbce1fb13
dori <rladbswls_06@naver.com>
2025-01-24 16:42:10 +0900

    [CHORE] #20 버튼 레이아웃 위치 조정

--- momogum/Home/View/StoryView.swift
+++ momogum/Home/View/StoryView.swift
@@ -17,6 +17,7 @@ struct StoryView: View {
                 .font(.title)
                 .bold()
                 .padding(.top, 218)
+            
             Text("당신의 한 끼를 기록하고, 공유해주세요 :)")
                 .padding(.top, 1)
                 .padding(.bottom, 92)
@@ -34,33 +35,32 @@ struct StoryView: View {
                     .scaledToFit()
                     .frame(width: 95, height: 95)
             }
+            
             NavigationLink(destination: GalleryPickerView()) {
-                // 밥일기 작성 버튼
-                NavigationLink(destination: GalleryPickerView()) {
-                    Text("바로 밥일기 작성하기")
-                        .font(.headline)
-                        .foregroundColor(Color(red: 224/255, green: 90/255, blue: 85/255)) // E05A55 색상
-                        .padding()
-                        .frame(width: 312, height: 52)
-                        .background(Color.clear)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color(red: 224/255, green: 90/255, blue: 85/255), lineWidth: 2)
-                        )
-                }
-                .padding(.top, 114)
-                
-                Spacer()
+                Text("바로 밥일기 작성하기")
+                    .font(.headline)
+                    .foregroundColor(Color(red: 224/255, green: 90/255, blue: 85/255)) // E05A55 색상
+                    .padding()
+                    .frame(width: 312, height: 52)
+                    .background(Color.clear)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color(red: 224/255, green: 90/255, blue: 85/255), lineWidth: 2)
+                    )
             }
-            .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        presentationMode.wrappedValue.dismiss() // 뒤로가기 동작
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .foregroundColor(.black)
-                    }
+            .padding(.top, 114)
+            
+            Spacer() // 아래쪽에 빈 공간 추가
+        }
+        .frame(maxWidth: .infinity) // VStack의 최대 너비를 설정하여 중앙 정렬
+        .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss() // 뒤로가기 동작
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.black)
                 }
             }
         }
